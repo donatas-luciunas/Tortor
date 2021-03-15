7f32724b4d110e3094da991107018bd4dfceb1f5
Donatas Lučiūnas <donatas@toughlex.com>
2021-03-15 15:05:03 +0200

    Clean code

--- sample/sum.js
+++ sample/sum.js
@@ -4,18 +4,26 @@ module.exports = async function (a, b) {
             // a++;
         }
     }
-    const c = a + b;
-    const z = 'testas';
-    const d = { mano: 4.20, kitas: { o: 3 }, a: [1, 2, 3], d: null, e: true, f: 'testas' };
-    const h = [1, 2, 3, 4, 5];
+    const sum = a + b;
+    const sampleString = 'testas';
+    const sampleObject = { mano: 4.20, kitas: { o: 3 }, a: [1, 2, 3], d: null, e: true, f: 'testas' };
+    console.log(sum, sampleString, sampleObject);
+
+    const sampleArray = [1, 2, 3, 4, 5];
     // await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log(c, z, d);
+    for (const item of sampleArray) {
+        const a = item + 1;
+        console.log(a);
+    }
     if (global.tortor) {
-        global.tortor.ti(null, null, () => {
-            console.log(global.tortor.runner.test.title);
+        // global.tortor.ti(null, null, () => {
+        //     console.log(global.tortor.runner.test.title);
+        // });
+        global.tortor.ti(null, 'mano ilgas testas', () => {
+            global.tortor.assert.equal(sum, 3);
         });
         global.tortor.ti(null, 'mano trumpas testas', () => {
-            global.tortor.assert.equal(c, 7);
+            global.tortor.assert.equal(sum, 7);
         });
     }
 };
